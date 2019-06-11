9df2d925d1bb9903a63b817c41cc483704f43099
Matthias Opitz <opitz@gmx.de>
2019-06-11 14:40:36 +0100

    added confirmation modal dialog (raw), need to rewrite commands handling

--- amd/src/execute.js
+++ amd/src/execute.js
@@ -3,8 +3,129 @@ define(['jquery', 'core/str', 'core/modal_factory', 'core/modal_events'], functi
         init: function() {
 
 // ---------------------------------------------------------------------------------------------------------------------
-            // Moving section0 to the ontop area
+            var executor = function(returnurl, courseid, command) {
+                // get the selected sections
+                var sections = [];
+                $('input[class="section"]:checked').each(function() {
+                    sections.push($(this).val());
+                    console.log($(this).val());
+                });
+                sections = JSON.stringify(sections);
+                console.log(sections);
+
+                var params = '';
+
+                // check for tab moves and extract the tab nr
+                if (command.indexOf('move2tab') > -1) {
+                    params = command.substr(command.length -1);
+                    command = 'move2tab';
+                }
+                // now execute the command with the selected sections
+                $.ajax({
+                    url: "execute.php",
+                    type: "POST",
+                    data: {'courseid': courseid, 'command': command, 'params': params, 'sections': sections},
+                    success: function(result) {
+                        if(result !== '') {
+                            console.log('Execution result:\n' + result);
+                            window.location = returnurl;
+                        }
+                    }
+                });
+            };
+// ---------------------------------------------------------------------------------------------------------------------
             var execute = function() {
+                $(".dropdown-item").on('click', function() {
+
+                    // get the course ID
+                    var courseid = $('#courseid').val();
+                    var returnurl = $('#returnurl').val();
+                    var commandStr = $(this).html();
+                    console.log('course ID = ' + courseid);
+
+                    // get the selected command
+//                    var command = $('#command option:selected').val();
+                    var command = $(this).attr('value');
+                    if(command === '') {
+                        $('#message').html('Please select an Action');
+                    } else {
+                        var trigger = $('.dropdown-item');
+                        ModalFactory.create({
+                            title: 'Please confirm',
+                            body: '<p>Do you really want to do this?</p>',
+                            type: ModalFactory.types.SAVE_CANCEL,
+                        }, trigger)
+                            .done(function(modal) {
+                                // Do what you want with your new modal.
+                                modal.getRoot().on(ModalEvents.save, function(e) {
+                                    // When modal "Save" button has been pressed.
+                                    e.preventDefault();
+                                    console.log('executing now...');
+                                    executor(returnurl, courseid, command);
+                                });
+                            });
+
+
+
+                    }
+                });
+
+                $("#btn_cancel").on('click', function() {
+                    window.location = $('#returnurl').val();
+                });
+
+            };
+            var execute0 = function() {
+                $(".dropdown-item").on('click', function() {
+
+                    // get the course ID
+                    var courseid = $('#courseid').val();
+                    var returnurl = $('#returnurl').val();
+                    console.log('course ID = ' + courseid);
+
+                    // get the selected command
+//                    var command = $('#command option:selected').val();
+                    var command = $(this).attr('value');
+                    if(command === '') {
+                        $('#message').html('Please select an Action');
+                    } else {
+                        // get the selected sections
+                        var sections = [];
+                        $('input[class="section"]:checked').each(function() {
+                            sections.push($(this).val());
+                            console.log($(this).val());
+                        });
+                        sections = JSON.stringify(sections);
+                        console.log(sections);
+
+                        var params = '';
+
+                        // check for tab moves and extract the tab nr
+                        if (command.indexOf('move2tab') > -1) {
+                            params = command.substr(command.length -1);
+                            command = 'move2tab';
+                        }
+                        // now execute the command with the selected sections
+                        $.ajax({
+                            url: "execute.php",
+                            type: "POST",
+                            data: {'courseid': courseid, 'command': command, 'params': params, 'sections': sections},
+                            success: function(result) {
+                                if(result !== '') {
+                                    console.log('Execution result:\n' + result);
+                                    window.location = returnurl;
+                                }
+                            }
+                        });
+                    }
+                });
+
+                $("#btn_cancel").on('click', function() {
+                    window.location = $('#returnurl').val();
+                });
+
+            };
+            var execute00 = function() {
                 $("#btn_execute").on('click', function() {
 
                     // get the course ID
@@ -14,44 +135,84 @@ define(['jquery', 'core/str', 'core/modal_factory', 'core/modal_events'], functi
 
                     // get the selected command
                     var command = $('#command option:selected').val();
+                    console.log('---------');
+                    console.log(command);
+                    console.log('---------');
 
-                    // get the selected sections
-                    var sections = [];
-                    $('input[class="section"]:checked').each(function() {
-                        sections.push($(this).val());
-                        console.log($(this).val());
-                    });
-                    sections = JSON.stringify(sections);
-                    console.log(sections);
+                    if(command === '') {
+                        $('#message').html('Please select an Action');
+                    } else {
+                        // get the selected sections
+                        var sections = [];
+                        $('input[class="section"]:checked').each(function() {
+                            sections.push($(this).val());
+                            console.log($(this).val());
+                        });
+                        sections = JSON.stringify(sections);
+                        console.log(sections);
 
-                    var params = '';
+                        var params = '';
 
-                    // check for tab moves and extract the tab nr
-                    if (command.indexOf('move2tab') > -1) {
-                        params = command.substr(command.length -1);
-                        command = 'move2tab';
-                    }
-                    // now execute the command with the selected sections
-                    $.ajax({
-                        url: "execute.php",
-                        type: "POST",
-                        data: {'courseid': courseid, 'command': command, 'params': params, 'sections': sections},
-                        success: function(result) {
-                            if(result !== '') {
-                                console.log('Execution result:\n' + result);
-                                window.location = returnurl;
-                            }
+                        // check for tab moves and extract the tab nr
+                        if (command.indexOf('move2tab') > -1) {
+                            params = command.substr(command.length -1);
+                            command = 'move2tab';
                         }
-                    });
+                        // now execute the command with the selected sections
+                        $.ajax({
+                            url: "execute.php",
+                            type: "POST",
+                            data: {'courseid': courseid, 'command': command, 'params': params, 'sections': sections},
+                            success: function(result) {
+                                if(result !== '') {
+                                    console.log('Execution result:\n' + result);
+                                    window.location = returnurl;
+                                }
+                            }
+                        });
+                    }
+                });
 
+                $("#btn_cancel").on('click', function() {
+                    window.location = $('#returnurl').val();
+                });
 
+            };
+
+// ---------------------------------------------------------------------------------------------------------------------
+            var droptest = function() {
+                $(".dropdown-item0").on('click', function() {
+                    alert($(this).attr('value'));
+                });
+            };
+
+// ---------------------------------------------------------------------------------------------------------------------
+            var modaltest = function() {
+                $("#btn_test").on('click', function() {
+//                    alert('test button clicked');
+                    var trigger = $('#btn_test');
+                    ModalFactory.create({
+                        title: 'test title',
+                        body: '<p>test body content</p>',
+                        type: ModalFactory.types.SAVE_CANCEL,
+                    }, trigger)
+                        .done(function(modal) {
+                            // Do what you want with your new modal.
+                            modal.getRoot().on(ModalEvents.save, function(e) {
+                                // When modal "Save" button has been pressed.
+                                e.preventDefault();
+                                console.log('modal saving here..');
+                            });
+                        });
                 });
             };
 
 // ---------------------------------------------------------------------------------------------------------------------
             var initFunctions = function() {
                 // Load all required functions above
+                modaltest();
                 execute();
+                $('.dropdown-toggle').dropdown();
             };
 
 // ---------------------------------------------------------------------------------------------------------------------
--- section_list.php
+++ section_list.php
@@ -10,9 +10,13 @@ global $PAGE;
 global $DB;
 global $COURSE;
 
+$courseid = $_GET['courseid'];
+$context = context_course::instance($courseid);
+$course = $DB->get_record('course', array('id' => $courseid));
+
 $PAGE->set_url('/local/bulkactions/view.php', array('id' => $cm->id));
-$PAGE->set_title('Bulk Sections Commands');
-$PAGE->set_heading('Bulk Sections Commands');
+$PAGE->set_title($course->fullname.' - Bulk Actions');
+$PAGE->set_heading($course->fullname.' - Bulk Actions');
 $PAGE->set_pagelayout('standard');
 
 //======================================================================================================================
@@ -20,23 +24,23 @@ $PAGE->requires->js_call_amd('local_bulkactions/execute','init', array());
 echo $OUTPUT->header();
 
 require_login();
-if (is_siteadmin()) {
+if ($PAGE->user_is_editing() and has_capability('moodle/course:update', $context)) {
+//if (is_courseadmin()) {
 
 
-
-    $courseid = $_GET['courseid'];
 
     $fo_records = $DB->get_records('course_format_options', array('courseid' => $courseid));
     $fo = array();
     foreach($fo_records as $for) {
         $fo[$for->name] = $for->value;
     }
-    echo "Select an action for course ID $courseid from the drop down menu, check the sections for the selected action and click on execute.<br><br>";
 
+    echo html_writer::tag('div','Check the topics you want the activity to apply to. Then select an action from the drop down menu.');
+    echo html_writer::empty_tag('br');
+
+    // build the commands array
     $commands = array();
-    $commands['Please select an action'] = 'test_sections';
-    $commands['Hide sections'] = 'hide_sections';
-    $commands['Show sections'] = 'show_sections';
+//    $commands['Please select an action'] = '';
 
     $maxtabs = (int)$fo['maxtabs'];
 // tab commands
@@ -44,6 +48,8 @@ if (is_siteadmin()) {
         $commands['Move to Tab '.$i] = 'move2tab'.$i;
     }
 
+    $commands['Hide sections'] = 'hide_sections';
+    $commands['Show sections'] = 'show_sections';
 
     $returnurl = $_SERVER['HTTP_REFERER'];
 
@@ -51,16 +57,59 @@ if (is_siteadmin()) {
     if($courseid) {
         echo "<input id='courseid' type='text' value='$courseid' style='display:none;'>";
         echo "<input id='returnurl' type='text' value='$returnurl' style='display:none;'>";
-        echo "<select id='command' name='command'>";
-        foreach($commands as $key => $command) {
-            echo "<option value='".$command."'>$key</option>";
-        }
-        echo "</select><hr>";
 
+        echo html_writer::start_tag('button', array('type' => 'button', 'id'=>'command', 'class' => 'btn dropdown-toggle', 'data-toggle' => 'dropdown'));
+        echo "Select Action";
+        echo html_writer::end_tag('button');
+
+        echo html_writer::start_tag('div', array('class' => 'dropdown-menu'));
+        foreach($commands as $key => $command) {
+            echo html_writer::tag('a', $key, array('class' => 'dropdown-item','value' => $command));
+//            "<option value='".$command."'>$key</option>";
+        }
+        echo html_writer::end_tag('div');
+
+        echo ' ';
+        echo '<input id="btn_cancel" class="btn" type="button" value="Cancel">';
+
+        echo ' ';
+        echo '<input id="btn_test" class="btn" type="button" value="Test">';
+
+        /*
+                echo "<select id='command' class='form-control0' type='button' name='command'>";
+                foreach($commands as $key => $command) {
+                    echo "<option value='".$command."'>$key</option>";
+                }
+                echo "</select>";
+                echo "<div id='message'></div><hr>";
+        */
 
         $sections = $DB->get_records('course_sections', array('course' => $courseid));
         $sql = "select * from {course_format_options} where courseid = $courseid and name like 'tab_'";
         $cfos = $DB->get_records_sql($sql);
+
+//        echo html_writer::start_tag('div', array('class' => 'container-fluid'));
+
+        echo html_writer::start_tag('table',array('class' => 'table table-striped ', 'border' => '0'));
+        echo html_writer::start_tag('thead');
+        echo html_writer::start_tag('tr');
+
+        echo html_writer::start_tag('th');
+        echo "Apply";
+        echo html_writer::end_tag('th');
+        echo html_writer::start_tag('th');
+        echo "Topic";
+        echo html_writer::end_tag('th');
+        echo html_writer::start_tag('th');
+        echo "Tab Position";
+        echo html_writer::end_tag('th');
+        echo html_writer::start_tag('th');
+        echo "Visibility";
+        echo html_writer::end_tag('th');
+
+        echo html_writer::end_tag('tr');
+        echo html_writer::end_tag('thead');
+        echo html_writer::start_tag('tbody');
 
         foreach($sections as $section){
             $sectionname = ($section->name == '' ? 'Section '.$section->section : $section->name);
@@ -68,20 +117,50 @@ if (is_siteadmin()) {
             $tablocation = '';
             foreach($cfos as $cfo){
                 if(strstr($cfo->value, $section->id)){
-                    $tablocation = " | Tab ".substr($cfo->name, -1)." ";
+                    $tablocation = "Tab ".substr($cfo->name, -1)." ";
                     break;
                 }
             }
-            echo '<input type="checkbox" class="section" value="'.$section->id.'" name="'.$section->section.'"> '.$sectionname.$hidinghint.$tablocation.'</input><br>';
+
+            echo html_writer::start_tag('tr');
+            echo html_writer::start_tag('td');
+//            echo html_writer::tag('input', array('type' => 'checkbox', 'class' => 'section', 'value' => $section->id, 'name' => $section->section));
+            echo '<input type="checkbox" class="section" value="'.$section->id.'" name="'.$section->section.'"> ';
+            echo html_writer::end_tag('td');
+            echo html_writer::start_tag('td');
+            echo $sectionname;
+            echo html_writer::end_tag('td');
+            echo html_writer::start_tag('td');
+            echo $tablocation;
+            echo html_writer::end_tag('td');
+            echo html_writer::start_tag('td');
+            echo $hidinghint;
+            echo html_writer::end_tag('td');
+            echo html_writer::end_tag('tr');
+
+//            echo '<input type="checkbox" class="section" value="'.$section->id.'" name="'.$section->section.'"> '.$sectionname.$hidinghint.$tablocation.'</input><br>';
         }
+
+        echo html_writer::end_tag('tbody');
+        echo html_writer::end_tag('table');
+
+//        echo html_writer::end_tag('div');
+
         echo "<br>";
-        echo '<input id="btn_execute" type="button" value="Execute">';
-        echo ' ';
-        echo '<input type="button" value="Cancel">';
+//        echo html_writer::start_tag('button', array('id' => 'btn_execute', 'class' => 'btn'));
+//        echo "Execute";
+//        echo html_writer::end_tag('button');
+//        echo '<input id="btn_execute" class="btn" type="button" value="Execute">';
+//        echo ' ';
+//        echo html_writer::start_tag('button', array('id' => 'btn_cancel', 'class' => 'btn'));
+//        echo "Cancel";
+//        echo html_writer::end_tag('button');
+//        echo '<input id="btn_cancel" class="btn" type="button" value="Cancel">';
     }
     echo "</form>";
 
 } else {
     echo(get_string('login_required', 'local_pluginmeta'));
 }
+
 echo $OUTPUT->footer();
